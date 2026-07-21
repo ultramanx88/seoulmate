882df8d0ebedcd1a6e1d75bad42c077bdb8cdfe9
Thanest Boontap <ultramanx88@gmail.com>
2026-07-21 09:40:45 +0700

    Drop Firebase UID constraint before column cleanup

--- server/migrations/006_drop_legacy_user_auth.sql
+++ server/migrations/006_drop_legacy_user_auth.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS oauth_states;
 DROP TABLE IF EXISTS auth_sessions;
 DROP TABLE IF EXISTS firebase_migration_records;
+ALTER TABLE users DROP CONSTRAINT IF EXISTS users_firebase_uid_key;
 DROP INDEX IF EXISTS users_firebase_uid_key;
 ALTER TABLE users DROP COLUMN IF EXISTS firebase_uid;
 ALTER TABLE users ALTER COLUMN auth_provider SET DEFAULT 'clerk';
